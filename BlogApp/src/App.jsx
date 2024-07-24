0310d994150d088e37bf81f285e681325d12987b
“Akansha <akshug1210@gmail.com>
2024-07-24 22:49:42 +0530

    redux store

--- BlogApp/src/App.jsx
+++ BlogApp/src/App.jsx
@@ -1,12 +1,40 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authService from './appwrite/auth'
+import {login,logout} from './store/authSlice'
+import {Header} from './components/index'
+import {Footer} from './components/index'
 import './App.css'
 
 function App() {
+  const [loading,setLoading]=useState(true)
+  const dispatch=useDispatch();
 
-  return (
-   <>
-   <h1>A blog App with Appwrite</h1>
-   </>
-  )
+
+ useEffect(()=>{
+  authService.getCurrentUser()
+  .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=> setLoading(false))
+ },[])
+
+ return !loading? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+  <div className='w-full block'>
+    <Header/>
+    <main>
+      Todo:{/*outlet*/}
+    </main>
+    <Footer/>
+  </div>
+  </div>
+ ) : (null)
 }
 
 export default App
