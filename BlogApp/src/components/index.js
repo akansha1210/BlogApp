5800427dfb73d4f36be8a5ed41f3b89c7be81187
“Akansha <akshug1210@gmail.com>
2024-08-09 00:48:55 +0530

    login page component2

--- BlogApp/src/components/index.js
+++ BlogApp/src/components/index.js
@@ -3,12 +3,19 @@ import Footer  from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './Logo';
 import LogoutBtn from './Header/LogoutBtn'
-
+import Input from './Input'
+import Select from './Select'
+import PostCard from './PostCard'
+import Button from './Button'
 
 export {
     Header,
     Footer,
     Container,
     Logo,
-    LogoutBtn
+    LogoutBtn,
+    Input,
+    Select,
+    PostCard,
+    Button
 }
