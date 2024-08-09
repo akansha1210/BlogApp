7b030674e6a2056b689bf99493fa4be99406bbba
“Akansha <akshug1210@gmail.com>
2024-08-09 14:20:53 +0530

    Signup Page

--- BlogApp/src/components/Signup.jsx
+++ BlogApp/src/components/Signup.jsx
@@ -1,46 +1,44 @@
 import React,{useState} from 'react'
-import {Link,useNavigate} from 'react-router-dom'
-import {login as authLogin} from '../store/store'
-import {Button, Input,Logo} from './index'
+import authService from '../appwrite/auth'
+import  {useNavigate, Link} from 'react-router-dom'
+import {login} from '../store/authSlice'
+import {Button,Logo,Input } from './index'
+import { useForm } from 'react-hook-form'
 import { useDispatch } from 'react-redux'
-import authService from "../appwrite/auth"
-import {useForm} from 'react-hook-form'
 
-
-function Login() {
-    const navigate= useNavigate()
+function Signup() {
+    const navigate=useNavigate()
     const dispatch = useDispatch()
+    const [error,setError]=useState("")
     const {register,handleSubmit}=useForm()
-    const [error,setError]=useState('')
 
-    const login = async(data)=>{
+
+    const create= async(data)=>{
         setError("")
-    try {
-       const session= await authService.login(data)
-       if(session){
-        const userData = await authService.getCurrentUser()
-        if(userData) dispatch(authLogin(userData))
-            navigate("/")
-       }
-    } catch (error) {
-        setError(error.message)
-    }    
+        try {
+            const userData =await authService.createAccount(data)
+            if(userData){ 
+                const userData=await authService.getCurrentUser()}
+                if(userData) dispatch(login(userData));
+                navigate('/')
+        } catch (error) {
+            setError(error.message)
+        }
     }
-    
+
 
 
   return (
-    <div className='flex items-center justify-center w-full'>
-         <div className={`mx-auto w-full max-w-lg bg-gray-100 
-            rounded-xl p-10 border border-black/10`}>
-            <div className="mb-2 flex justify-center">
-                    <span className="inline-block w-full max-w-[100px]">
+    <div className="flex items-center justify-center">
+         <div className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}>
+         <div className="mb-2 flex justify-center">
+         <span className="inline-block w-full max-w-[100px]">
                         <Logo width="100%" />
-                    </span>
-        </div>
-        <h2 className="text-center text-2xl font-bold leading-tight">Sign in to your account</h2>
+        </span>
+         </div>
+         <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
         <p className="mt-2 text-center text-base text-black/60">
-                    Don&apos;t have any account?&nbsp;
+                    Already have an account?&nbsp;
                     <Link
                         to="/signup"
                         className="font-medium text-primary transition-all duration-200 hover:underline"
@@ -50,8 +48,15 @@ function Login() {
         </p>
         {error && <p className='text-red-600 mt-8 text-center'>{error}</p>}
         <form
-        onSubmit={handleSubmit(login)} className='mt-8'>
+        onSubmit={handleSubmit(create)} >
             <div className='space-y-5'>
+                <Input
+                label="Name: "
+                placeholder="Enter your full name"
+                {...register,("name",{
+                    required:true
+                })}
+                />
                 <Input label="Email: "
                 placeholder="write your email"
                 type="email"
@@ -71,15 +76,14 @@ function Login() {
                     required:true
                 })}
                 />
-                <Button
+                 <Button
                 type='submit'
-                className='w-full'>Sign In</Button>
+                className='w-full'>Create Account</Button>
             </div>
         </form>
-
          </div>
     </div>
   )
 }
 
-export default Login
+export default Signup
